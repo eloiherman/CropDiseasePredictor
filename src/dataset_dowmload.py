7f969cafe8cd30478be1a5fbd836e73fc051eb98
coucou330 <jeanflichy1@gmail.com>
2026-03-09 22:32:55 -0400

    dataset_acquaintance

---
+++ src/dataset_dowmload.py
@@ -0,0 +1,7 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("/OneDrive - McGill University/Cours/Winter 2026/mais 202/repo/CropDiseasePredictor/crop-health-and-environmental-stress-dataset")
+
+print("Path to dataset files:", path)
+
